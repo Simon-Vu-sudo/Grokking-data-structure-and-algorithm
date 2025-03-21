e57b309d13c41fa41ea0dea5c3cb1eeb0c094fe8
Hòa Vũ <hoaminhvuu@gmail.com>
2025-03-21 22:27:25 +0700

    Convert to Typescript

---
+++ twoPointer/squareOfSortedArr.ts
@@ -0,0 +1,18 @@
+function numSubarrayProductLessThanK(nums: number[], k: number): number {
+  if(k < 1) return 0; 
+  let windowProduct = 1;
+  let count = 0, right = 0, left = 0;
+
+  while(right < nums.length) {
+
+      windowProduct *= nums[right];
+      while(windowProduct >= k) {
+          windowProduct /= nums[left];
+          left++;
+      }
+      
+      count += right - left + 1;
+      right++;
+  }
+  return count;
+};
